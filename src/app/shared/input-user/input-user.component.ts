35b7170409172d0371f73145fa558e21465cd5bf
Vanderlei Klug da Silva <vanderleis47@gmail.com>
2024-09-17 20:31:16 -0300

    tests

--- src/app/shared/input-user/input-user.component.ts
+++ src/app/shared/input-user/input-user.component.ts
@@ -1,18 +1,27 @@
 import { Component, NgModule } from '@angular/core';
 import { Service } from '../service.service';
 import { FormsModule, NgModel } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
+import { CommonModule, DecimalPipe, I18nPluralPipe, JsonPipe, TitleCasePipe } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-input-user',
   standalone: true,
-  imports: [FormsModule, JsonPipe],
+  imports: [FormsModule, JsonPipe, I18nPluralPipe, TitleCasePipe, DecimalPipe, CommonModule, NgbModule],
   templateUrl: './input-user.component.html',
   styleUrl: './input-user.component.scss'
 })
 export class InputUserComponent {
 
-  newvalue: form = {value:'', date:''}
+  count: { [k: string]: string} = {
+    '=0': 'ano',
+    '=1': 'ano',
+    'other': 'anos'
+  }
+
+  price = 585.52
+  percent = 58.50
+  newvalue: form = {age: 0, value:'', date:''}
   constructor(public service: Service){
 
   }
@@ -25,6 +34,7 @@ export class InputUserComponent {
 }
 
 interface form {
+  age: number
   value: string
   date: string
 }
